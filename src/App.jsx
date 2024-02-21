44c2f9046b440d5e722586388d3f37688ea706bb
jonathan bushapa <jonathanbushapa@Mac-mini-7.home>
2024-02-21 13:52:16 -0600

    added table

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
 import './App.css'
+// import './components/ContactList'
+import ContactList from './components/ContactList';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
+    
+       <ContactList />
   
+     
     </>
-  )
+  );
 }
 
 export default App
